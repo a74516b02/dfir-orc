c5f67b087bf7e0a857e54d5ec53149681244dbb3
a74516b02 <157634829+a74516b02@users.noreply.github.com>
2024-01-27 17:39:52 +0100

    Update build.ps1

--- tools/ci/build.ps1
+++ tools/ci/build.ps1
@@ -82,7 +82,7 @@ function Build-Orc
         [Parameter()]
         [ValidateSet('static', 'dynamic', IgnoreCase=$false)]
         [String]
-        $Runtime = 'dynamic',
+        $Runtime = 'static',
         [Parameter()]
         [System.IO.DirectoryInfo]
         $Vcpkg,
